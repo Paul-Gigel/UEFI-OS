e52cfb279add1925152d7c930f842020f671c417
Paul Gigel <p.gigel@bbw-fi.de>
2023-09-09 17:40:54 +0200

    leel

---
+++ UEFI-OSbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#rm -rf edk2/
+#mkdir edk2
+#sudo apt install build-essential git python2 uuid-dev nasm acpica-tools
+#git clone https://github.com/tianocore/edk2.git
+cd edk2
+#git submodule update --init
+#export EDK_TOOLS_PATH=$PWD/BaseTools
+source edksetup.sh
+make -C BaseTools
+. edksetup.sh
+cd ..
